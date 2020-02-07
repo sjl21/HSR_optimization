fefa965f4230ec76bcbaaf8ca841203ab55b9d04
sjl21 <60325566+sjl21@users.noreply.github.com>
2020-02-07 12:43:04 -0500

    Update OR_9.py

--- OR_9.py
+++ OR_9.py
@@ -9,17 +9,13 @@ Created on Sun Feb  2 11:59:10 2020
 
 # Github
 
-import os
 from gurobipy import Model, GRB
-import pulp
-from scipy.optimize import linprog
 import pandas as pd
 import numpy as np
 import math
-from tkinter import *
 from IPython import get_ipython
 import itertools
-import functools
+import tkinter as tk
 
 
 get_ipython().magic('reset -sf')
@@ -30,14 +26,14 @@ price_HSR = pd.read_excel('price_hsr.xlsx')
 duration_AIR = pd.read_excel('duration_air.xlsx')
 price_AIR = pd.read_excel('price_air.xlsx')
 
-print(price_AIR)
 
-
+cities = ['New York',	'Boston',	'Philadelphia',	'Providence',	'D.C.',	'Baltimore',	'Portland']
 
 duration_HSR = np.array(duration_HSR)
 price_HSR = np.array(price_HSR)
 duration_AIR = np.array(duration_AIR)
 price_AIR = np.array(price_AIR)
+capacity_HSR = 15000*np.ones((7,7))
 
 
 cost_HSR = duration_HSR*100
@@ -60,6 +56,8 @@ services = np.zeros((7,7))
 city1 = range(7)
 city2 = range(7)
 
+#Itineraries- passengers should be able to take multiple services to reach a destination in the system. 
+
 
 # Optimization
     
@@ -79,6 +77,8 @@ m.addConstrs((actual[c1,c2] == demand[c1,c2]*total_passengers[c1,c2]-spill+recap
 
 m.addConstrs((revenue[c1,c2] == price_HSR[c1,c2]*actual[c1,c2]*open[c1,c2] for c1,c2 in itertools.product(range(7), range(7))), "revenue")
 
+m.addConstrs((actual[c1,c2] <= capacity_HSR[c1,c2] for c1,c2 in itertools.product(range(7), range(7))), "capacity")
+
 # Objective
 obj = sum(revenue[c1,c2]-cost_HSR[c1,c2]*open[c1,c2] for c1,c2 in itertools.product(range(7), range(7)))
 m.setObjective(obj, GRB.MAXIMIZE) # maximize profit
@@ -89,11 +89,8 @@ m.optimize()
 
 for x in range(7):
     for y in range(7):
-        if(x==y):
-            print('Service %s,%s does not exist' %(x,y))
-        else:
-            if(open[x,y].x > 0.99):
-                print('Service %s,%s is open' %(x,y))
-            else:
-                print('Service %s,%s is closed' %(x,y))
+        if(open[x,y].x > 0.99):
+            print( '%s to %s is open' %(cities[x],cities[y]))
+            print('Passenger Count: %s' %round(actual[x,y].x))
+            print()
 
